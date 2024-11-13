cee8139aeead27668b1831e64aac8dd9ef41c38f
Edmondo Silvestri <Edmondo.Silvestri@ptvgroup.com>
2024-11-13 09:16:36 +0100

    enrich test, implementation is bugged

--- QuadTree.Tests/QuadTreeCellTests.cs
+++ QuadTree.Tests/QuadTreeCellTests.cs
@@ -7,12 +7,28 @@ namespace QuadTree.Tests;
 public class QuadTreeCellTests
 {
 
-  [TestMethod]
-  public void FindNearest()
+  [DataTestMethod]
+  [DataRow(1000, 0.1)]
+  [DataRow(10000, 0.1)]
+  [DataRow(100000, 0.1)]
+  [DataRow(1000, 1.0)]
+  [DataRow(10000, 1.0)]
+  [DataRow(100000, 1.0)]
+  [DataRow(1000, 10.0)]
+  [DataRow(10000, 10.0)]
+  [DataRow(100000, 10.0)]
+  public void FindNearest(int numberOfPoints, double size)
   {
     var random = new Random(42);
-    var points = Enumerable.Range(0, 1000).Select(_ => Vector128.Create(random.NextDouble(), random.NextDouble())).ToList();
-    var testPoint = Vector128.Create(random.NextDouble(), random.NextDouble());
+    var points = Enumerable
+      .Range(0, numberOfPoints)
+      .Select(_ => Vector128.Create(
+        random.NextDouble() * size, 
+        random.NextDouble() * size))
+      .ToList();
+    var testPoint = Vector128.Create(
+      random.NextDouble() * size, 
+      random.NextDouble() * size);
     var expected = points.OrderBy(p => VectorExtensions.DistanceSquared(p, testPoint)).First();
     var tree = QuadTree.BuildQadTree(points);
     var nearest = tree.FindNearest(testPoint);
